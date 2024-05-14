2173defdbc00f01fd6ca50844079168f4a68c86f
sadu vishwanath <153756418+Vishu-reddy@users.noreply.github.com>
2024-05-14 11:41:48 +0530

    add database

--- app.js
+++ app.js
@@ -3,70 +3,65 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _=require("lodash");
+const _ = require("lodash");
+const mongoose=require("mongoose");
+
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const posts=[];
+
+mongoose.connect("mongodb://localhost:27017/BlogDb", {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+});
+const postSchema={
+  title:String,
+  content:String
+};
+const Post=mongoose.model("Post",postSchema);
 const app = express();
-
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.get("/",async function(req, res){
+   const posts=await Post.find({},'title content').exec();
+    res.render("home",{
+      startingContent: homeStartingContent,
+      posts: posts
+    });  
+});
 
-app.get("/",(req,res)=>{
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
 
-  res.render("home",{
-    para1:homeStartingContent,
-    posts:posts
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post =new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
   });
-
-})
-app.get("/about",(req,res)=>{
-  res.render("about",{
-    about:aboutContent
+  post.save().then(()=>{
+    console.log("saved");
   })
-})
-app.get("/contact",(req,res)=>{
-  res.render("contact",{
-    contact:contactContent
-});
-})
-app.get("/compose",(req,res)=>{
-  res.render("compose")
-});
-
-app.post("/compose",(req,res)=>{
-  const post={
-    title:req.body.text,
-    body:req.body.content
-  }
-  posts.push(post);
   res.redirect("/");
+
 });
-app.get("/posts/:post",(req,res)=>{
-  let p=_.lowerCase(req.params.post);
-  let found=false;
-  posts.forEach(function(post){
-    // Object.values(post).forEach(function(value){
-      var value=_.lowerCase(post.title);
-      if(value===p){
-        found=true;
-        res.render("post",{
-          title:post.title,
-          content:post.body,
-        })
-      }
-    // })
-  })
+app.get("/posts/:postId", async function(req, res){
+  const requestedId = req.params.postId;
+  const post=await Post.findOne({_id:requestedId},'title content').exec();
+  res.render("post", {
+          title: post.title,
+          content: post.content
+        });
 });
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
